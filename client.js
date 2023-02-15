b613726a9ece1c438211a3a90274b15e7aa2a999
Liz Fosdick <lizfosdick@gmail.com>
2023-02-15 14:05:07 -0800

    adding play.js, updated client.js

---
+++ client.js
@@ -0,0 +1,21 @@
+// establishes a connection with the game server
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: "50541",
+  });
+
+  conn.on("data", (messageFromTheServer) => {
+    console.log(messageFromTheServer);
+  });
+  
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = { connect };
