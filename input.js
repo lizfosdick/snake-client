5b4ae25c277818d33b8558839a5ba4ee66679e9d
Liz Fosdick <lizfosdick@gmail.com>
2023-02-15 17:06:38 -0800

    added message

--- input.js
+++ input.js
@@ -29,6 +29,9 @@ const handleUserInput = function (key) {
   if (key === 'd') {
     connection.write("Move: right");
   }
+  if (key === 'i') {
+    connection.write("Say: good job!");
+  }
 };
 
 
